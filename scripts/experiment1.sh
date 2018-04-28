2f7061f5983192a0676c394f5a9ea93d5f4ace66
Christopher Canel <c.canel@icloud.com>
2018-04-28 22:22:59 +0000

    Add experiment scripts

---
+++ scripts/experiment1.sh
@@ -0,0 +1,17 @@
+VIDEO="/home/ubuntu/mnt/data/vimba_iii_1_2018-3-21_7-224-short.mp4"
+LABELS="../labeler/iii_01_buses/iii_01_7"
+BATCH=16
+PCT_FRAMES=0.8
+EPOCHS=5
+
+for WINDOW in 1 16 32;
+do
+  echo "[INFO] Window "$WINDOW
+  python train.py --window $WINDOW \
+                  --video $VIDEO \
+                  --eval-type rgb \
+                  --labels-dir $LABELS \
+                  --batch $BATCH \
+                  --epochs $EPOCHS \
+                  --pct-frames $PCT_FRAMES
+done
