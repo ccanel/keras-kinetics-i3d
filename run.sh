36cb0401dd187ecf8f81a2cbe833027bd1c200c5
Christopher Canel <c.canel@icloud.com>
2018-04-28 22:13:17 +0000

    Run with 1 minimum frame rate

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 VIDEO="/home/ubuntu/mnt/data/vimba_iii_1_2018-3-21_7-224-short.mp4"
-WINDOW=8
 LABELS="../labeler/iii_01_buses/iii_01_7"
 BATCH=16
-PCT_FRAMES=0.2
-EPOCHS=2
+PCT_FRAMES=0.4
+EPOCHS=3
+WINDOW=2
+
+echo "[INFO] Window "$WINDOW
 python train.py --window $WINDOW \
                 --video $VIDEO \
                 --eval-type rgb \
