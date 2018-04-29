7287a801d6129ff7b8c2da0deee96a759ff72a54
Christopher Canel <c.canel@icloud.com>
2018-04-29 20:00:10 +0000

    Improve logging

--- measure_deploy.py
+++ measure_deploy.py
@@ -25,22 +25,34 @@ def parse_args():
         "--samples", help="Num samples", type=int, required=False, default=5)
     parser.add_argument(
         "--out-file", help="Output file", type=str, required=True)
+    parser.add_argument(
+        "--fps", help="Frames per second of the camera stream", type=int,
+        required=False, default=30)
     return parser.parse_args()
 
 
-def record_result(out_file, window_size, dur):
-    msg = "{},{}".format(window_size, dur)
-    print("Result: {}".format(msg))
+def record_result(out_file, window_size, inf_dur_s, fps):
+    acc_dur_s = float(window_size) / fps
+    total_dur_s = acc_dur_s + inf_dur_s
+    msg = "{},{},{},{}".format(
+        window_size, acc_dur_s, inf_dur_s, total_dur_s)
+    print(
+        ("Window size: {} , Accumulate time: {:.3f} s , "
+         "Inference time: {:.3f} s , Total time: {:.3f} s").format(
+             window_size, acc_dur_s, inf_dur_s, total_dur_s))
     out_file.write("{}\n".format(msg))
 
 
 def main():
     args = parse_args()
 
+    fps = args.fps
     samples = args.samples
     print("Number of samples: {}".format(samples))
     with open(args.out_file, "w") as out_file:
-        out_file.write("# Window size, time (ms)")
+        out_file.write(
+            ("# Window size, accumulate time (s), inference time (s), total "
+             "time (s)"))
 
         for window_size in args.window:
             print("Timing window size: {}".format(window_size))
@@ -59,12 +71,14 @@ def main():
             one_sample = raw_samples[0][0]
             all_samples = np.asarray([[one_sample] * window_size])
 
-            start_time = time.time()
+            start_time_s = time.time()
             for _ in xrange(samples):
                 # make prediction
                 model.predict(all_samples)
-            dur = (time.time() - start_time) / float(samples)
-            record_result(out_file, window_size, dur)
+                inf_dur_s = (time.time() - start_time_s) / float(samples)
+
+
+            record_result(out_file, window_size, inf_dur_s, fps)
     return
 
 
