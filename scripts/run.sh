506d99f034ba64de9f6bf09c7e6923437c7c1d5a
Christopher Canel <c.canel@icloud.com>
2018-04-29 01:24:29 +0000

    Add support for red cars

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,15 @@
-VIDEO="/home/ubuntu/mnt/data/vimba_iii_1_2018-3-21_7-224-short.mp4"
-LABELS="../labeler/iii_01_buses/iii_01_7"
+#!/bin/bash
+task=$1
+if [[ $1 == "redcar" ]]; then
+  VIDEO="/home/ubuntu/mnt/data/scaled_vimba_iii_1_2018-3-21_16.mp4-224-short.mp4"
+  LABELS="../labeler/iii_01_redcar/iii_01_16"
+elif [[ $1 == "bus" ]]; then
+  VIDEO="/home/ubuntu/mnt/data/vimba_iii_1_2018-3-21_7-224-short.mp4"
+  LABELS="../labeler/iii_01_buses/iii_01_7"
+else
+  echo "You must specify either bus or redcar"
+  exit
+fi
 BATCH=16
 PCT_FRAMES=0.4
 EPOCHS=3
